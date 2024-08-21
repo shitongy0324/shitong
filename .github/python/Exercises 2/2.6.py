255ce5492b6e0c4a438e06f589b5f59bae4c5d13
shitong0324 <shitongy@metropolia.fi>
2024-08-21 11:00:42 +0300

    Create blank.yml

---
+++ .github/python/Exercises 2/2.6.py
@@ -0,0 +1,20 @@
+import random
+list1 = []
+list2 = []
+i=1
+n = 1
+while i<= 3:
+    a=random.randint(0,9)
+    list1.append(a)
+    i+=1
+print(list1)
+while n<=4:
+    b=random.randint(1,6)
+    list2.append(b)
+    n+=1
+print(list2)
+
+
+
+
+
