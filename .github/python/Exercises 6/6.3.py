b3f8ba512d209fcd4e06839b55c90a9e26687086
shitong0324 <shitongy@metropolia.fi>
2024-08-27 13:00:39 +0300

    Create blank.yml

---
+++ .github/python/Exercises 6/6.3.py
@@ -0,0 +1,13 @@
+def liter(gallon):
+    float(gallon)
+    return gallon * 3.785
+
+while True:
+    user_gallon = float(input("input gallons"))
+    if user_gallon<0:
+        print("stop")
+        break
+    else:
+        user_liter=liter(user_gallon)
+        print(f"{user_gallon:.2f}gallons={user_liter:.2f}liters")
+        continue
