f66f50cda57563b619df02affd9c50eb9eb35279
shitong0324 <shitongy@metropolia.fi>
2024-11-10 13:55:46 +0200

    .

--- .github/JAVA/module-1/1.3/1-3.js
+++
@@ -1,8 +0,0 @@
-const number_1 = parseInt(prompt('enter the first number'))
-const number_2 = parseInt(prompt('enter the second number'))
-const number_3 = parseInt(prompt('enter the third munber'))
-
-const sum = number_1 + number_2 + number_3
-const product = number_1 * number_2 * number_3
-const average = sum / 3
-document.querySelector('#target').innerHTML = `The sum of three numbers is ${sum}\nThe product of three numbers is ${product}\nThe average of three number is ${average}`
